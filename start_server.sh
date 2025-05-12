4f6502ab19feceb6f8f69dd693a50b9428222cb4
Richard Gomes <rgomes.info@gmail.com>
2025-05-12 13:00:43 +0400

    Review build scripts

--- start_server.sh
+++ start_server.sh
@@ -6,5 +6,9 @@ set -eu
 
 cargo install basic-http-server
 
+# kills existing running server, if any
+pid=$(ss -plnt | grep basic-http-serv | grep -P -o '(?<=pid=)[0-9]+')
+[[ -z "${pid}" ]] || kill "${pid}"
+
 (cd docs && basic-http-server --addr 127.0.0.1:8080 .)
 # (cd docs && python3 -m http.server 8080 --bind 127.0.0.1)
